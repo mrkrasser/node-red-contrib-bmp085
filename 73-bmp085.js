c1e69579e3ef986659689670bd4dd3068b9d71d0
Maxwell Hadley <mrh.github@susato.demon.co.uk>
2015-04-03 21:14:37 +0100

    Added user-selectable measurement units Added correction to sea level Updated help test to match

--- 73-bmp085.js
+++ 73-bmp085.js
@@ -24,6 +24,22 @@ module.exports = function(RED) {
         // node configuration
 		this.device = config.device;
 		this.timer = config.timer * 1000;
+		this.name = config.name;
+		this.pressureUnits = config.pressureUnits || "mmHg";
+		this.temperatureUnits = config.temperatureUnits || "degC";
+		if (this.pressureUnits === "hPa" || this.pressureUnits === "mBar") {
+			this.pressureFactor = 1.0;
+		} else if (this.pressureUnits === "mmHg") {
+			this.pressureFactor = 0.75006;
+		} else if (this.pressureUnits === "inHg") {
+			this.pressureFactor = 0.0295299;
+		} else {
+			this.pressureFactor = 1.0;
+		}
+		this.seaLevel = config.seaLevel || "QFE";
+		// Note: height units are only used in the editor: config.height is always in m
+		this.height = config.height || 0;
+		this.heightUnits = config.heightUnits || "m";
 		var node = this;
 		
 		var sensor = new BMP085({
@@ -35,7 +51,14 @@ module.exports = function(RED) {
 		var getData = function(){
 			// TODO: error handling
 			sensor.read(function (data) {
-				data.pressure = (data.pressure/1.3332239).toFixed();
+				// data.pressure = (data.pressure/1.3332239).toFixed();
+				if (node.seaLevel === "QNH") {
+					data.pressure *= Math.pow(288.15/(288.15-0.0065*node.height), 5.255876);
+				}
+				data.pressure *= node.pressureFactor;
+				if (node.temperatureUnits === "degF") {
+					data.temperature = 32 + data.temperature*1.8;
+				}
 				var msg = { payload: data };
 				node.send(msg);
 			});	
